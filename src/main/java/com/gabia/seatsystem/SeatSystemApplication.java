70196b32c1717aded703c37da9b2ae2ff96fbc1c
june <jhbang8917@gmail.com>
2018-08-18 14:27:11 +0900

    first commit

---
+++ src/main/java/com/gabia/seatsystem/SeatSystemApplication.java
@@ -0,0 +1,12 @@
+package com.gabia.seatsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatSystemApplication.class, args);
+    }
+}
